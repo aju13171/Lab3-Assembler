e0c3c69667fab50391382e5eb47bda60eb9b7a8d
aju13171 <c.valentinaju@gmail.com>
2015-08-22 09:48:48 -0600

    solo se agregaron las subrutinas para el set del pin y la funcion

--- generador.s
+++ generador.s
@@ -17,6 +17,9 @@ Astable:
 apagado de 0.5s del led ok 3 veces. Se mantendra apagado
 .globl Fail
 Fail:
+	/*encendido del led OK (pin 16)*/
+	/*se realiza el set del pin*/
+	
 	
 	
 
@@ -44,7 +47,7 @@ fin:
 	
 @@**************************************************
 @@subrutinas Wait, GetTimeStamp, GetSystemTimerBase
-@@tomadas del ejemplo ok4 
+@@tomadas del ejemplo ok04 del taller de Assembler
 @@**************************************************
 
 
@@ -90,7 +93,94 @@ GetSystemTimerBase:
 	ldr r0,=0x20003000
 	mov pc,lr
 	
+@@****************************************************
+@@subrutinas GetGpioAddress, setGpioFunction, SetGpio
+@@tomadas del ejemplo ok04 del taller de Assembler
+@@****************************************************
+
+@@Se agrega la funcion al pin del GPIO dado por R0
+.globl SetGpioFunction
+SetGpioFunction:
+    pinNum .req r0
+    pinFunc .req r1
+	cmp pinNum,#53
+	cmpls pinFunc,#7
+	movhi pc,lr
+
+	push {lr}
+	mov r2,pinNum
+	.unreq pinNum
+	pinNum .req r2
+	bl GetGpioAddress
+	gpioAddr .req r0
+
+	functionLoop$:
+		cmp pinNum,#9
+		subhi pinNum,#10
+		addhi gpioAddr,#4
+		bhi functionLoop$
+
+	add pinNum, pinNum,lsl #1
+	lsl pinFunc,pinNum
+
+	mask .req r3
+	mov mask,#7					/* r3 = 111 in binary */
+	lsl mask,pinNum				/* r3 = 11100..00 where the 111 is in the same position as the function in r1 */
+	.unreq pinNum
+
+	mvn mask,mask				/* r3 = 11..1100011..11 where the 000 is in the same poisiont as the function in r1 */
+	oldFunc .req r2
+	ldr oldFunc,[gpioAddr]		/* r2 = existing code */
+	and oldFunc,mask			/* r2 = existing code with bits for this pin all 0 */
+	.unreq mask
+
+	orr pinFunc,oldFunc			/* r1 = existing code with correct bits set */
+	.unreq oldFunc
+
+	str pinFunc,[gpioAddr]
+	.unreq pinFunc
+	.unreq gpioAddr
+	pop {pc}
 	
+@@Envia la señal (encendido/apagado) puesto en R1 al pin del GPIO enviado por R0
+.globl SetGpio
+SetGpio:	
+    pinNum .req r0
+    pinVal .req r1
+
+	cmp pinNum,#53
+	movhi pc,lr
+	push {lr}
+	mov r2,pinNum	
+    .unreq pinNum	
+    pinNum .req r2
+	bl GetGpioAddress
+    gpioAddr .req r0
+
+	pinBank .req r3
+	lsr pinBank,pinNum,#5
+	lsl pinBank,#2
+	add gpioAddr,pinBank
+	.unreq pinBank
+
+	and pinNum,#31
+	setBit .req r3
+	mov setBit,#1
+	lsl setBit,pinNum
+	.unreq pinNum
+
+	teq pinVal,#0
+	.unreq pinVal
+	streq setBit,[gpioAddr,#40]
+	strne setBit,[gpioAddr,#28]
+	.unreq setBit
+	.unreq gpioAddr
+	pop {pc}
 	
-	
-	
+@@Retorna la direccion del GPIO en R0
+.globl GetGpioAddress
+GetGpioAddress: 
+	gpioAddr .req r0
+	ldr gpioAddr,=0x20200000
+	mov pc,lr
+	.unreq gpioAddr
